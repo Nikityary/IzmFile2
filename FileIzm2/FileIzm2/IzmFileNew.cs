a249e82f30969ba00b3c7d8843329d371518469e
Никита <112887529+Nikityary@users.noreply.github.com>
2022-09-29 18:36:27 +0300

    Add files via upload

---
+++ FileIzm2/FileIzm2/IzmFileNew.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace FileIzm2
+{
+    internal class IzmFileNew
+    {
+        public string java(string path = @"C:\Users")
+        {
+            FileInfo g = new FileInfo(path);
+            string nl = ($"{g.LastWriteTime}");
+            return nl;
+        }
+    }
+}
---
+++ FileIzm2/FileIzm2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileIzm2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IzmFileNew h = new IzmFileNew();
+            h.java();
+        }
+    }
+}
